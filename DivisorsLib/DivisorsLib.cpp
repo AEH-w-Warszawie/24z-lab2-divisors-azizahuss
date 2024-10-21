17fe65e3c898998605f8424829ca75ba22f71e4d
azizahuss <1aziz.2005aziza@gmail.com>
2024-10-21 15:15:25 +0400

    Updated APP (Faster)

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,25 @@
 #include <vector>
+#include <cmath>
+#include <algorithm>
 #include "Divisors.h"
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    if (number == 0) return result; 
+
+    for (unsigned long long i = 1; i <= static_cast<unsigned long long>(std::sqrt(number)); ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i); 
+            if (i != number / i) 
+            {
+                result.push_back(number / i); 
+            }
+        }
+    }
+
+    std::sort(result.begin(), result.end()); 
+    return result;
 }
